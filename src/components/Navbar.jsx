3bcdbf12e7f0ef2d11b52f0e38022f77cf75f5e9
new name <email for my second account>
2024-02-18 21:46:39 +0600

    first commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div className=' bg-slate-800 py-5 px-20 rounded-b-full fixed w-full'>
+    <div className=' bg-blue1 py-5 px-20 rounded-b-full fixed w-full z-50'>
         <div className='flex justify-between items-center'>
             <div>
                 <Link to="/"><img src={Logo} className='h-42 w-20 cursor-pointer'/></Link>
@@ -14,7 +14,7 @@ const Navbar = () => {
                 <li className='mx-4'><Link className='font-semibold text-lg text-white font-poppins' to="#">Services</Link></li>
                 <li className='mx-4'><Link className='font-semibold text-lg text-white font-poppins' to="/portfolio">Portfolio</Link></li>
                 <li className='mx-4'><Link className='font-semibold text-lg text-white font-poppins' to="/contact">Contact</Link></li>
-                <li className='mx-4'><Link  className=' py-2 px-6 rounded-lg font-bold text-medium text-white' style={{backgroundImage :"linear-gradient(170deg, #fdcd3b 75%, #ffffff 25%)"}}>Career</Link></li>
+                <li className='mx-4'><Link  className=' py-2 px-6 rounded-lg font-bold text-medium text-white' style={{backgroundImage :"linear-gradient(170deg, #AC00FD 75%, #ffffff 25%)"}}>Career</Link></li>
             </div>
         </div>
     </div>
